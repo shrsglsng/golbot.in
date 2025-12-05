c8d69a5127c653e4e381977dff34a2f6a67eb9b5
nvrakesh06 <hsekarwtf@gmail.com>
2025-12-05 09:57:01 +0530

    fix imports

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ import morgan from "morgan";
 import mongoSanitize from "express-mongo-sanitize";
 import helmet from "helmet";
 
+import "./config/demoConfig.js";
+
 // Import utilities
 import logger from "./utils/logger.js";
 import { responseMiddleware } from "./utils/response.js";
